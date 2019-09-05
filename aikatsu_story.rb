828ab4d280e3d33cd9890b510c86a4d778c44cfc
gecko655 <aqwsedrft1234@yahoo.co.jp>
2019-09-06 01:13:01 +0900

    Publish

--- aikatsu_story.rb
+++ aikatsu_story.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class AikatsuStory < Formula
   homepage "https://github.com/gecko655/homebrew-aikatsu-story"
-  url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.3.zip"
+  url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.4.zip"
   sha256 "f66e09de73926194924716914502a833bb7a16f95ae90b3e7ddc9a22480d0f0a"
   head "https://github.com/gecko655/homebrew-aikatsu-story"
-  version "0.3"
+  version "0.4"
 
   depends_on "pup"
   depends_on "jq"
