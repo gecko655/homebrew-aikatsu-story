35b2cce992765d3e4e58d9c61a3f4951c94dbb69
gecko655 <aqwsedrft1234@yahoo.co.jp>
2019-10-04 00:40:59 +0900

    Added option --url --title

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # id of the last episode of Aikatsu Friends! As of 2019-09-06
-FRIENDS_LAST_EP=74
+FRIENDS_LAST_EP=76
 # id of the last episode of Aikatsu on Parade! As of 2019-09-06
 ONPARADE_LAST_EP=1
 
@@ -7,8 +7,9 @@ ex_code=0
 for i in `seq 1 178`
 do
   story=`./aikatsu_story -0 $i`
-  echo "Aikatsu!$i: $story"
-  if [[ -z $story ]];then
+  title=`./aikatsu_story -0 --title $i`
+  echo "Aikatsu!$i: $title $story"
+  if [[ -z $title || -z $story ]];then
     echo "Failed to fetch story of Aikatsu! ep$i" 1>&2
     ex_code=1
   fi
@@ -16,17 +17,19 @@ done
 for i in `seq 1 100`
 do
   story=`./aikatsu_story -s $i`
-  echo "Aikatsu Stars!$i: $story"
-  if [[ -z $story ]];then
+  title=`./aikatsu_story -s --title $i`
+  echo "Aikatsu Stars!$i: $title $story"
+  if [[ -z $title || -z $story ]];then
     echo "Failed to fetch story of Aikatsu Stars! ep$i" 1>&2
     ex_code=1
   fi
 done
-for i in `seq 1 $FRIENDS_LAST_EP`
+for i in `seq 1 76`
 do
   story=`./aikatsu_story -f $i`
-  echo "Aikatsu Friends!$i: $story"
-  if [[ -z $story ]];then
+  title=`./aikatsu_story -f --title $i`
+  echo "Aikatsu Friends!$i: $title $story"
+  if [[ -z $title || -z $story ]];then
     echo "Failed to fetch story of Aikatsu Friends! ep$i" 1>&2
     ex_code=1
   fi
@@ -35,8 +38,9 @@ done
 for i in `seq 1 $ONPARADE_LAST_EP`
 do
   story=`./aikatsu_story -o $i`
-  echo "Aikatsu on Parade!$i: $story"
-  if [[ -z $story ]];then
+  title=`./aikatsu_story -o --title $i`
+  echo "Aikatsu on Parade!$i: $title $story"
+  if [[ -z $title || -z $story ]];then
     echo "Failed to fetch story of Aikatsu on Parade! ep$i" 1>&2
     ex_code=1
   fi
