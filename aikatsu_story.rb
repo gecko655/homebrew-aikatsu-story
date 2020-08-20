1ed0288f72877228b324f774e28b4d842f2fd3af
gecko655 <aqwsedrft1234@yahoo.co.jp>
2020-08-20 23:18:54 +0900

    fix sha256

--- aikatsu_story.rb
+++ aikatsu_story.rb
@@ -3,7 +3,7 @@ require "formula"
 class AikatsuStory < Formula
   homepage "https://github.com/gecko655/homebrew-aikatsu-story"
   url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.8.zip"
-  sha256 "2efe3a4de34f8aba2d9c9479f6ebdcb21aa3295eb569c31f4f0e6224261d21d3"
+  sha256 "05fb28ffec857a62c417a53417bcfca7cbd5cd36473a4ac3685d84e937f3d749"
   # curl -L https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.x.zip | shasum -a 256 -
   head "https://github.com/gecko655/homebrew-aikatsu-story"
   version "0.8"
