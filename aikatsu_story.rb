60cbddd709a46d2e83bf3b2114cbab43acc3f972
YAMAMORI, Akihiro <6166778+gecko655@users.noreply.github.com>
2021-01-11 17:42:31 +0900

    Update version

--- aikatsu_story.rb
+++ aikatsu_story.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class AikatsuStory < Formula
   homepage "https://github.com/gecko655/homebrew-aikatsu-story"
-  url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.8.zip"
-  sha256 "05fb28ffec857a62c417a53417bcfca7cbd5cd36473a4ac3685d84e937f3d749"
+  url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.9.3.zip"
+  sha256 "b8ba6d188f8ad00cd32e029749b8e82d5e9059837ab072e7117f9afebbfaf8f6"
   # curl -L https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.x.zip | shasum -a 256 -
   head "https://github.com/gecko655/homebrew-aikatsu-story"
-  version "0.8"
+  version "0.9.3"
 
   depends_on "pup"
   depends_on "jq"
