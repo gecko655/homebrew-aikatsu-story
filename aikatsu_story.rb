1a153f47f8cb869d4027ac9ce6301d065898a407
YAMAMORI, Akihiro <6166778+gecko655@users.noreply.github.com>
2023-11-21 20:01:55 +0900

    update hash

--- aikatsu_story.rb
+++ aikatsu_story.rb
@@ -3,8 +3,8 @@ require "formula"
 class AikatsuStory < Formula
   homepage "https://github.com/gecko655/homebrew-aikatsu-story"
   url "https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.9.4.zip"
-  sha256 "b8ba6d188f8ad00cd32e029749b8e82d5e9059837ab072e7117f9afebbfaf8f6"
-  # curl -L https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.x.zip | shasum -a 256 -
+  sha256 "781268007074cf2995fb46f92e68fe8909059cf07f8bf821e16d5b99f9b17576"
+  # curl -s -L https://github.com/gecko655/homebrew-aikatsu-story/archive/v0.x.zip | shasum -a 256 -
   head "https://github.com/gecko655/homebrew-aikatsu-story"
   version "0.9.4"
 
